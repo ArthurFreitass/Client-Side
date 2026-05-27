89921906b5ef96facdf760b2fdc268f50ef46169
Arthur Freitas <freitarthur176@gmail.com>
2026-05-27 11:07:36 -0300

    Add files via upload

---
+++ HTML_CSS_JS/diaDois/exercicios/Um/script.js
@@ -0,0 +1,11 @@
+/*Atividade 1 — Soma Simples
+Crie um programa que peça dois números ao usuário e mostre a soma deles
+utilizando alert().*/
+
+number1 = prompt("Digite um número:")
+number1 = Number.parseInt(number1)
+
+number2 = prompt("Digite um número:")
+number2 = Number.parseInt(number2)
+
+alert("A soma dos dois números é: "+(number1+ number2))
