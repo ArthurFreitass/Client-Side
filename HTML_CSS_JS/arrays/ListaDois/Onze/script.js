321bd819276020928f4b330844d70d3dc96c0261
Arthur Freitas <freitarthur176@gmail.com>
2026-06-08 10:32:34 -0300

    Delete HTML_CSS_JS/arrays directory

--- HTML_CSS_JS/arrays/ListaDois/Onze/script.js
+++
@@ -1,7 +0,0 @@
-let arrayNums = []
-
-for (let i = 0; i < 5; i++) {
-    arrayNums.push(Number.parseFloat(prompt("Digite um número:")))
-}
-
-alert("Os valores na ordem inversa são: "+ arrayNums.reverse())
--- HTML_CSS_JS/arrays/ListaDois/Quinze/script.js
+++
@@ -1,8 +0,0 @@
-let array = []
-let mult = 1
-for (let i = 0; i < 5; i++) {
-    array.push(Number.parseInt(prompt("Digite um número:")))
-    mult *= array[i]
-}
-
-alert("A multiplicação de todos os números é = "+mult)
