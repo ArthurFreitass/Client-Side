721af8df00c69b421b82614d7b3e34c245bc79cf
Arthur Freitas <freitarthur176@gmail.com>
2026-06-09 10:07:43 -0300

    Update script.js

--- HTML_CSS_JS/arrays/ListaTres/Quinze/script.js
+++ HTML_CSS_JS/arrays/ListaTres/Quinze/script.js
@@ -1,45 +1,13 @@
 const largest = () => {
     const arr = []
 
-    alert("Entre com o vetor:")
-
     for (let i = 0; i < 5; i++) {
         arr.push(Number(prompt("Digite um número:")))
     }
 
-    let biggest = arr[0]
+    arr.sort((a, b) => b - a)
 
-    arr.forEach((numArr) => {
-        if (numArr > biggest) {
-            biggest = numArr
-        }
-    })
-
-    let lessNumber
-
-    if (biggest != arr[0]) {
-        lessNumber = arr[0]
-        arr.forEach((nums) => {
-            if (nums < lessNumber) {
-                lessNumber = nums
-            }
-        })
-    } else {
-        lessNumber = arr[1]
-        arr.forEach((nums) => {
-            if (nums < lessNumber) {
-                lessNumber = nums
-            }
-        })
-    }
-
-    let secondLargest = lessNumber;
-
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < biggest && arr[i] > secondLargest) {
-            secondLargest = arr[i]
-        }
-    }
+    const secondLargest = arr[1]
 
     alert("O segundo maior é = " + secondLargest)
 }
